8e6a0cd8e7a79109ea14a874314f384963291990
Alex Ungurianu <alex.ungurianu@protonmail.com>
2023-12-13 18:57:17 +0000

    day13: part 2

--- src/days/13/solve.zig
+++ src/days/13/solve.zig
@@ -24,8 +24,16 @@ fn getCol(lines: std.ArrayList([]u8), col_idx: usize) ![]u8 {
     return col;
 }
 
-fn solveSquare(lines: std.ArrayList([]u8)) !u64 {
+const Res = struct {
+    mirror: u64,
+    mirror_cand: u64,
+};
+
+fn solveSquare(lines: std.ArrayList([]u8)) !Res {
     const line_len = lines.items[0].len;
+
+    var mirror: ?u64 = null;
+    var mirror_cand: ?u64 = null;
 
     for (1..line_len) |can_i| {
         const to_l = can_i;
@@ -33,39 +41,62 @@ fn solveSquare(lines: std.ArrayList([]u8)) !u64 {
 
         const p_l = @min(to_l, to_r);
 
-        var is_sol = true;
+        var unmatch_count: u32 = 0;
         for (lines.items) |line| {
             if (!isPalindrome(line[(can_i - p_l)..(can_i + p_l)])) {
-                is_sol = false;
-                break;
+                unmatch_count += 1;
+                if (unmatch_count > 1) {
+                    break;
+                }
             }
         }
-        if (is_sol) {
-            return can_i;
+        if (unmatch_count == 0) {
+            mirror = can_i;
+        }
+        if (unmatch_count == 1) {
+            mirror_cand = can_i;
+        }
+        if (mirror != null and mirror_cand != null) {
+            break;
         }
     }
 
-    const row_len = lines.items.len;
-    for (1..row_len) |can_i| {
-        const to_l = can_i;
-        const to_r = row_len - can_i;
+    if (mirror == null or mirror_cand == null) {
+        const row_len = lines.items.len;
 
-        const p_l = @min(to_l, to_r);
+        for (1..row_len) |can_i| {
+            const to_l = can_i;
+            const to_r = row_len - can_i;
 
-        var is_sol = true;
-        for (0..line_len) |col_i| {
-            const line = try getCol(lines, col_i);
-            defer lines.allocator.free(line);
-            if (!isPalindrome(line[(can_i - p_l)..(can_i + p_l)])) {
-                is_sol = false;
+            const p_l = @min(to_l, to_r);
+
+            var unmatch_count: u32 = 0;
+            for (0..line_len) |col_i| {
+                const line = try getCol(lines, col_i);
+                defer lines.allocator.free(line);
+                if (!isPalindrome(line[(can_i - p_l)..(can_i + p_l)])) {
+                    unmatch_count += 1;
+                    if (unmatch_count > 1) {
+                        break;
+                    }
+                }
+            }
+            if (unmatch_count == 0) {
+                mirror = can_i * 100;
+            }
+            if (unmatch_count == 1) {
+                mirror_cand = can_i * 100;
+            }
+            if (mirror != null and mirror_cand != null) {
                 break;
             }
         }
-        if (is_sol) {
-            return can_i * 100;
-        }
     }
-    return 0;
+
+    return .{
+        .mirror = mirror.?,
+        .mirror_cand = mirror_cand.?,
+    };
 }
 
 pub fn solve(allocator: std.mem.Allocator, file: std.fs.File) anyerror!bp.AoCResult {
@@ -73,11 +104,14 @@ pub fn solve(allocator: std.mem.Allocator, file: std.fs.File) anyerror!bp.AoCRes
     var in_stream = buf_reader.reader();
 
     var part1: u64 = 0;
+    var part2: u64 = 0;
     var lines = std.ArrayList([]u8).init(allocator);
     defer lines.deinit();
     while (try in_stream.readUntilDelimiterOrEofAlloc(allocator, '\n', 1000)) |line| {
         if (line.len == 0) {
-            part1 += try solveSquare(lines);
+            const res = try solveSquare(lines);
+            part1 += res.mirror;
+            part2 += res.mirror_cand;
             for (lines.items) |line_to_free| {
                 allocator.free(line_to_free);
             }
@@ -88,12 +122,14 @@ pub fn solve(allocator: std.mem.Allocator, file: std.fs.File) anyerror!bp.AoCRes
     }
 
     if (lines.items.len > 0) {
-        part1 += try solveSquare(lines);
+        const res = try solveSquare(lines);
+        part1 += res.mirror;
+        part2 += res.mirror_cand;
         for (lines.items) |line_to_free| {
             allocator.free(line_to_free);
         }
         try lines.resize(0);
     }
 
-    return .{ .part1 = part1, .part2 = 0 };
+    return .{ .part1 = part1, .part2 = part2 };
 }
