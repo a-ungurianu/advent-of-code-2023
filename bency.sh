75c48a2bd0a87ed9b995f731076b342ed4267dbc
Alex Ungurianu <alex.ungurianu@protonmail.com>
2023-12-16 23:11:50 +0000

    tool: make benchy runnable for a specific day

--- bency.sh
+++ bency.sh
@@ -2,24 +2,44 @@ mkdir -p .benchy
 
 SOLVED_DAYS=15
 
-function profile () {
+function set_optimize() {
+    OPTIMIZE=$1;
+    zig build -Doptimize=$OPTIMIZE;
+}
+
+function profile_day() {
+    local day=$1;
+
+    mkdir -p ./.benchy/$day
+    local callgrind_file="./.benchy/$day/callgrind.$OPTIMIZE.out"
+    valgrind --tool=callgrind --callgrind-out-file=$callgrind_file ./zig-out/bin/advent-of-code-2023 $day
+    gprof2dot -f callgrind $callgrind_file | dot -Tsvg -o ./.benchy/$day/trace.$OPTIMIZE.svg
+}
+
+function profile_all () {
     zig build -Doptimize=$OPTIMIZE;
 
     echo "Hyperfine all days [$OPTIMIZE]";
-    # hyperfine --warmup 10 -N "./zig-out/bin/advent-of-code-2023 {day}" -P day 1 $SOLVED_DAYS --export-markdown .benchy/hp.md;
+    hyperfine --warmup 10 -N "./zig-out/bin/advent-of-code-2023 {day}" -P day 1 $SOLVED_DAYS --export-markdown .benchy/hp.md;
     echo "Callgrind days separately [$OPTIMIZE]";
     for day in $(seq 1 $SOLVED_DAYS); do
-        mkdir -p ./.benchy/$day
-        local callgrind_file="./.benchy/$day/callgrind.$OPTIMIZE.out"
-        valgrind --tool=callgrind --callgrind-out-file=$callgrind_file ./zig-out/bin/advent-of-code-2023 $day
-        gprof2dot -f callgrind $callgrind_file | dot -Tsvg -o ./.benchy/$day/trace.$OPTIMIZE.svg
+        profile_day $day
     done
         
 }
 
+if [ -z "$1" ]; then
+    
+    set_optimize Debug;
+    profile_all;
 
-# OPTIMIZE="Debug"
-# profile
+    set_optimize ReleaseSafe;
+    profile_all;
+else
+    set_optimize Debug;
+    profile_day $1;
 
-OPTIMIZE="ReleaseSafe"
-profile
+    set_optimize ReleaseSafe;
+    profile_day $1;
+
+fi
